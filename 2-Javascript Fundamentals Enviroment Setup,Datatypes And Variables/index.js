8f35a911461c0a871b89656dfb73e2cdd35d11a2
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-03-12 20:09:02 +0500

    Class 3- Fundamentals Of Javascript Enviroment Setup,Datatypes And Variables And Its Rules

---
+++ 2-Javascript Fundamentals Enviroment Setup,Datatypes And Variables/index.js
@@ -0,0 +1,148 @@
+//1-Enviroment Setup
+
+//Vs code is an  text-editor and IDE(Integreted Development Enviroment)
+// Vs code is generally Hybird of both
+//Download And Install VSCODE
+
+//Strongly Typed Langauges Statically Typed Languages
+//Explicitly Batana Parta Hai App Logo Ko There Are Two Things Explicit And Implicit
+//Java And C++
+// int a = 23;
+
+//Explicit And Implicit
+
+//Explicit
+//like java,c++ etc
+
+//int myVar = 42; //you are explicitly telling c++ or java to store only these type of values
+// myVar = "i am string now" this is not allowed
+//String myString = "I Am A String $ % ^ 42" //ANything inside these Single Or Double Quotes Are Called Strings
+//myString = 42; this is not allowed in staticly typed languages or strongly typed languages
+
+//Implicit
+
+//like javascript php and python
+
+// var myVar = 42; //javascript will understand on its own that it has cetain type of value you dont have to tell javscript
+//myVar  = "I AM A String" THIS IS Allowed in dynamically typed languages
+
+//2-Data Types
+
+// There Are 7 Datatypes In Javascript
+
+// 1-Number
+
+//-2 -1 0 1 2 These ARE Called Integers
+//-1.1 0.01 1.1 2.3 These Are Floating
+
+//THESE Both Are Numbers
+
+//2-String
+
+// 'Any thing inside double or single quotes is called string true false'
+
+//3-Boolean
+
+//true Or 1 true is also denoted as 1
+//false Or 0 and false is also denoted as 0
+
+//  var userIsLoggedIn = true ;
+
+// if(userIsLoggedIn){
+// Show him his feed
+// }else {
+// agar wo true nai hai toh ise print kardo
+// }
+//we will discuss if/else ya conditional logic soon
+
+//4- Null
+
+// there is nothing ,VOID Or empty;
+
+//  var nullExample = null;
+
+//5-undefined
+
+// undefined means variable is declared but not defined
+// var undefinedExample; i only declare it  but i didnot defined it javascript will impicitly add undefined to this variable
+//undefinedExample = undefined; you can explicityly defined it but dont need to define because javascript will define it on its own
+
+//6- Object
+// Objects Are king In Javascript everything is object in javascript
+// before understanding objects we have to understand arrays
+
+//Arrays
+
+//Variables In Javascript
+
+//Variable Is Like A Container/Box
+
+//How Variable Are Defined
+
+//in 3 ways we can define variables
+//var let and cost
+//let and const are part of Ecmascript 6/Ecmascript 2015
+//Ecmascript is basically javascript
+//we will not discuss let and const in this lecture
+
+//we will use var
+//YOU CAN GIVE Any name to your variable i gave myName
+//var myName= "Jeet Kumar";
+
+// Rules To Set Variable Name
+
+// if you dont follow rules then javascript will throw an error
+//convention: it is not necarry to follow this but if you follow it called as good practice
+
+// Rules
+
+// 1-variable name never be started with numbers
+
+// Dont's
+// var 1name you cant start variable name starting from a number
+// var for; reserved keywords for variable names are not allowed
+// var myname%; only few special characters are allowed in variable name like _ and $
+// var my name; space is not allowed inside variable name
+// Do's
+// var name1;
+//var $name;
+//var my_name
+//var myname;
+
+//Convention
+//var myName // good practice to give variable name in camleCase
+
+//variable is like a box where you store any value
+//you can then use that variable to manipulate your data
+//variable will represent that value
+
+// var myVar = "My nAME IS JEET";
+// console.log(myVar);
+// console.log(myVar);
+// console.log(myVar);
+// console.log(myVar);
+
+//Case Sensitive
+//java c++ javascript etc
+
+//Case Insensitive
+//php etc....
+
+// Case Sensitive Example
+// var myname = "Jeet Kumar";
+// console.log(myname);
+// console.log(myName); THIS IS WRONG In javascript it is not valid javascript is case sensitive language
+
+//Case Insenstive like php
+// php code example
+// $myvar = "jeet kumar"
+// print($myvar);
+// print($MyVar)
+// print($MYVAR)
+
+// THESE ARE VALID IN PHP BECAUSE IT IS CASE INSENSITIVE LANGUAGE
+
+
+
+
+
