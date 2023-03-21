1a4de1f1b418fd94525b82a6e8a3833436159d07
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-03-21 10:26:33 +0500

    javascript basics to advance course

---
+++ 4-Javascript Fundamentals Part 3 Operators And Its Types/script.js
@@ -0,0 +1,129 @@
+// 📚 Topics We Will Cover In Today's Class:
+
+// 👉 Operators and Different Types of Operators
+// 👉 typeOf Operator
+// 👉 Arithmetic Operators
+// 👉 Comparison Operators
+// 👉 Logical Operators
+// 👉 Assignemnt Operators
+// 👉 Unary Operators
+// 👉 Modulous Operator
+
+//  Operators and Different Types of Operators:
+// In programming, an operator is a symbol or keyword that performs a specific operation on one or more operands (values or variables). There are different types of operators, such as:
+
+// Arithmetic Operators: These operators are used to perform mathematical operations, such as addition, subtraction, multiplication, division, and modulus.
+
+// Comparison Operators: These operators are used to compare two values and return a boolean value (true or false) based on the comparison. Examples of comparison operators include greater than (>), less than (<), equal to (==), and not equal to (!=).
+
+// Logical Operators: These operators are used to combine two or more boolean expressions and return a boolean result. Examples of logical operators include AND (&&), OR (||), and NOT (!).
+
+// Assignment Operators: These operators are used to assign a value to a variable. Examples of assignment operators include equals (=), plus-equals (+=), and minus-equals (-=).
+
+// Unary Operators: These operators work on a single operand and include increment (++), decrement (--), and negation (-).
+
+// Ternary Operator: This operator is a shorthand way of writing an if-else statement in a single line.
+
+// var val_1 = 20;
+// var val_2 = 30;
+// var result = 2 + 2
+
+// typeOf Operator
+
+// The typeof operator is used to determine the data type of a value or variable in JavaScript. It returns a string indicating the data type of the operand. Examples of data types include number, string, boolean, undefined, and object.
+
+// var myName = {name: "shahzaib"}
+// var isOn = true
+// var onlyDeclare;
+// console.log(typeof onlyDeclare)
+
+// 👉 Arithmetic Operators
+
+// These operators are used to perform mathematical operations, such as addition, subtraction, multiplication, division, and modulus.
+
+// +
+// -
+// /
+// *
+// %
+
+// console.log("Addition:",2+2);
+// console.log("Subtraction:",2-2);
+// console.log("Division:"+2/2);
+// console.log("Multiplication:"+2*2);
+// console.log("Modulous Operator"+10%2); it give us reminder
+
+// 👉 Comparison Operators
+
+// These operators are used to compare two values and return a boolean value (true or false) based on the comparison. Examples of comparison operators include greater than (>), less than (<), equal to (==), and not equal to (!=).
+
+// console.log(3==2)
+// console.log(3>2)
+// console.log(3<2)
+// console.log(3!=2)
+// console.log(2<=3)
+// console.log(3>=3)
+
+// 👉 Logical Operators
+
+// These operators are used to combine two or more boolean expressions and return a boolean result. Examples of logical operators include AND (&&), OR (||), and NOT (!).
+
+// Agar Logical Operators Samjh Gaye Aap Log Toh Ifelse Mein Champion Hojaoge
+
+// AND &&
+// OR ||
+
+//     OR TABLE:
+//     true || true = true
+//     true || false = true
+//     false || true = true
+//     false || false = false
+//              T      T    = true
+// console.log(3>2 || 4<5)  = true
+
+//                 T      F    = true
+//   console.log(3>2 || 4>5)  = true
+
+// F      T       = true
+//   console.log(3<2 || 4<5)     = true
+// console.log(3==2 || false)
+// // console.log(true || false )
+// console.log(false || false )
+
+//     AND TABLE:
+//     true  && true = true
+//     true  && false = false
+//     false && true = false
+//     false && false = false
+
+// console.log(false && true)
+// console.log(true && false)
+// console.log(true && true)
+// console.log(false && true)
+
+// console.log(2>2  && 3<2)
+// console.log(2>=2 && 3<=2)
+// console.log(2>=2 && 3>=2)
+// console.log(2==2 && 3<=2)
+
+// 👉 Assignemnt Operators  (=)
+//  These operators are used to assign a value to a variable. Examples of assignment operators include equals (=), plus-equals (+=), and minus-equals (-=).
+
+// var myVar = 23
+//  console.log(myVar)
+// // myVar+= 2;
+// // // myVar = myVar + 2
+// //             // oldValue + newValue
+
+// // console.log(myVar)
+// myVar-= 3;
+// // myVar = myVar - 3
+// console.log(myVar);
+
+// 👉 Unary Operators
+
+// These operators work on a single operand and include increment (++), decrement (--), and negation (-).
+
+// var myValue = 23;
+// // console.log(++myValue)
+// // console.log(--myValue)
